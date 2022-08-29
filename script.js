5f0fae599ee75cbb5b4925c52148cb7485f5c057
Clara <claraswanson671@gmail.com>
2022-08-29 12:22:05 +0200

    Get Plotly scatter set up

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 "use strict"
 
-import Chart from 'chart.js/auto';
 
-const Chart = require('chart.js');
-const ctx = document.getElementById('launchChart').getContext('2d');
 
-const launchChart = new Chart(ctx,  {
-            type: "scatter",
-            data: {},
-            options: {}
-        });
+const launchChart = document.getElementById('launchChart');
+
+//// PLOTLY ////
+const successes = {
+    x: [1, 2, 3, 4],
+    y: [10, 15, 13, 17],
+    mode: 'markers',
+    type: 'scatter'
+};
+  
+const failures = {
+    x: [2, 3, 4, 5],
+    y: [16, 5, 11, 9],
+    mode: 'markers',
+    type: 'scatter'
+};
+
+const data = [successes, failures];
+
+const layout = {
+    title: 'SpaceX Launches',
+    showlegend: true,
+}
+
+Plotly.newPlot(launchChart, data, layout);
+/// END ///
