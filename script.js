4da1bb3b9f104c2d6a3fe4a4396ac432ba720b0f
Clara <claraswanson671@gmail.com>
2022-09-05 12:25:21 +0200

    Change scatter to bar and use api data

--- script.js
+++ script.js
@@ -4,6 +4,55 @@
 const launchChart = document.getElementById('launchChart');
 let successfulLaunches = []
 let failedLaunches = [];
+const yearOfSuccessfulLaunch = [];
+const yearOfFailedLaunch = [];
+const numOfSuccessfulLaunchEachYear = [];
+const numOfFailedLaunchEachYear = [];
+
+// [1, 1, 1, 1, 1, 8]
+
+//// PLOTLY ////
+const successes = {
+    x: yearOfSuccessfulLaunch,
+    y: numOfSuccessfulLaunchEachYear,
+    mode: 'markers',
+    name: 'successes',
+    marker: {size: 6, color: '#0f0'},
+    type: 'bar'
+};
+  
+const failures = {
+    x: yearOfFailedLaunch,
+    y: [1, 1, 1, 1, 1, 8],
+    mode: 'markers',
+    name: 'failures',
+    marker: {size: 6, color: '#f00'},
+    type: 'bar'
+};
+
+const data = [successes, failures];
+
+const layout = {
+    barmode: 'stack',
+    title: 'SpaceX Launches',
+    showlegend: true,
+    xaxis: {
+        title: 'year of launch',
+        titlefont: {size: 16},
+        range: [2004, 2024]
+    },
+    yaxis: {
+        title: '# of launches',
+        titlefont: {size: 16},
+        range: [0, 1500]
+    },
+}
+
+/// END ///
+function getOccurrence(array, value) {
+    return array.filter((v) => (v === value)).length;
+}
+
 
 /// API Request ///
 axios.get('https://api.spacexdata.com/v4/launches').then((res) => {
@@ -12,54 +61,41 @@ axios.get('https://api.spacexdata.com/v4/launches').then((res) => {
 
     for (let launch of allLaunches) {
 
-        if (launch.success === false) {
+        if (launch.success) {
+
             successfulLaunches.push(launch.name);
-        } else if (launch.success === true) {
+            let d = new Date(launch.date_local);
+            yearOfSuccessfulLaunch.push(d.getFullYear());
+
+        } else {
+
             failedLaunches.push(launch.name);
+            let d = new Date(launch.date_local);
+            yearOfFailedLaunch.push(d.getFullYear());
+            
         }
     }
+    
+    for (const year of yearOfSuccessfulLaunch) {
+        const numOfSuccessfulLaunches = getOccurrence(yearOfSuccessfulLaunch, year)
+        numOfSuccessfulLaunchEachYear.push(numOfSuccessfulLaunches);
+    }
 
-    console.log(successfulLaunches);
-    console.log(failedLaunches);
+    for (const year of yearOfFailedLaunch) {
+        const numOfFailedLaunches = getOccurrence(yearOfFailedLaunch, year)
+        numOfFailedLaunchEachYear.push(numOfFailedLaunches);
+    }
+
+    console.log('list of num of failed launches: ', numOfFailedLaunchEachYear);
+    console.log('successful launches: ', successfulLaunches);
+    console.log('failed launches: ', failedLaunches);
+    console.log('years of successful launches: ', yearOfSuccessfulLaunch);
+    console.log('years of failed launches: ', yearOfFailedLaunch);
 
 
 }).catch((err) => {
     console.log(err);
+}).then(() => {
+    Plotly.newPlot(launchChart, data, layout);
 })
-/// END ///
-
-
-//// PLOTLY ////
-const successes = {
-    x: [2006, 2009, 2016, 2022],
-    y: [10, 15, 13, 17],
-    mode: 'markers',
-    name: 'successes',
-    marker: {size: 6, color: '#0f0'},
-    type: 'scatter'
-};
-  
-const failures = {
-    x: [2006, 2009, 2016, 2022],
-    y: [16, 5, 11, 9],
-    mode: 'markers',
-    name: 'failures',
-    marker: {size: 6, color: '#f00'},
-    type: 'scatter'
-};
-
-const data = [successes, failures];
-
-const layout = {
-    title: 'SpaceX Launches',
-    showlegend: true,
-    xaxis: {
-        range: [2004, 2024]
-    },
-    yaxis: {
-        range: [0, 20]
-    },
-}
-
-Plotly.newPlot(launchChart, data, layout);
 /// END ///
